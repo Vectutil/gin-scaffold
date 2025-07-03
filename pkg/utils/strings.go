36fd8d73d359555b59e8724a2737ed895264504b
breelk <935549961@qq.com>
2025-07-03 17:51:10 +0800

    添加字符串分割去空方法

--- pkg/utils/strings.go
+++ pkg/utils/strings.go
@@ -1,6 +1,9 @@
 package utils
 
-import "strconv"
+import (
+	"strconv"
+	"strings"
+)
 
 func String2Uint64(str string) uint64 {
 	parseUint, _ := strconv.ParseUint(str, 10, 64)
@@ -9,4 +12,16 @@ func String2Uint64(str string) uint64 {
 func String2int64(str string) int64 {
 	parseUint, _ := strconv.ParseInt(str, 10, 64)
 	return parseUint
+}
+
+func SplitTrim(str, splitMark string) []string {
+	split := strings.Split(str, splitMark)
+	strs := make([]string, 0)
+	for _, v := range split {
+		tv := strings.TrimSpace(v)
+		if tv != "" {
+			strs = append(strs, tv)
+		}
+	}
+	return strs
 }
